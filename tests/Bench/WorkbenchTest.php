b06249d236cc6977c99d326a9158cf8a7a1f7a21
Vadim <enocryph@yandex.ru>
2018-09-08 00:36:19 +0300

    tests add

--- tests/Bench/WorkbenchTest.php
+++ tests/Bench/WorkbenchTest.php
@@ -11,7 +11,7 @@ use PHPUnit\Framework\TestCase;
 
 class WorkbenchTest extends TestCase
 {
-    public function testWorkbenchForStrings()
+    public function testWorkbenchForStringsWithSeparated()
     {
         $sequenceCreator = new StringSequenceCreator('-2..');
         $sequence = $sequenceCreator->getSequence();
@@ -32,5 +32,28 @@ class WorkbenchTest extends TestCase
         $workbench->cut();
         $workbench->applyOnSeparated('strtoupper');
         $this->assertSame('poweR', $workbench->getValue());
+    }
+
+    public function testWorkbenchForStringsWithOthers()
+    {
+        $sequenceCreator = new StringSequenceCreator('-2..');
+        $sequence = $sequenceCreator->getSequence();
+        $objectify = new ObjectifyString('power');
+
+        $scissors = new StringScissors();
+        $glue = new StringGlue();
+        $workbench = new Workbench($objectify, $sequence, $scissors, $glue);
+
+        $workbench->cut();
+        $workbench->applyOnOther('strtoupper');
+        $this->assertSame('POWer', $workbench->getValue());
+
+        $sequenceCreator = new StringSequenceCreator('-1');
+        $sequence = $sequenceCreator->getSequence();
+        $workbench = new Workbench($objectify, $sequence, $scissors, $glue);
+
+        $workbench->cut();
+        $workbench->applyOnOther('strtoupper');
+        $this->assertSame('POWEr', $workbench->getValue());
     }
 }
